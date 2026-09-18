2ce05ccc81df3d9eaadf8097d151d2abe525b793
ROBE <robinbertling10@gmail.com>
2026-09-18 11:46:05 +0200

    brokenExample

--- 1901/FuelCalculator.cs
+++ 1901/FuelCalculator.cs
@@ -38,7 +38,7 @@ namespace _1901
             int totalFuelWeight = 0;
             
 
-            while(currentFuelWeight > 2)
+            while(currentFuelWeight = 4)
             {
                 int currentFuelRequirment = GetRequiredFuel(currentFuelWeight);
                 if(currentFuelRequirment == 0)
